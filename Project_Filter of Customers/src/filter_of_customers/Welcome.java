c43b8edaa6a79cf99bbdcb74eb1eee224ef99b12
M05G07 <stanciaumetra@gmail.com>
2023-08-25 08:39:44 +0200

    Added Funcionality to Interface Welcome

--- Project_Filter of Customers/src/filter_of_customers/Welcome.java
+++ Project_Filter of Customers/src/filter_of_customers/Welcome.java
@@ -1,5 +1,16 @@
 package filter_of_customers;
 
 public interface Welcome {
-    
+    static void zobrazUvitacieMenu () {
+        System.out.println("\n" + "*************************" + "\n" +
+                "Vitajte; nacitajte si vybrany Zoznam Zakaznikov:" + "\n"
+        );
+
+        System.out.println( " -> Zoznam zakaznikov sluzby Hlasovy Pausal [pouzi volbu 1]");
+        System.out.println( " -> Zoznam zakaznikov sluzby Internet [pouzi volbu 2]");
+        System.out.println( " -> Zoznam zakaznikov na fakturu [pouzi volbu 3]");
+        System.out.println( " -> Zoznam zakaznikov s mobilnym cislom [pouzi volbu 4]");
+        System.out.println( " -> Ukonci Aplikaciu [pouzi volbu 5]" + "\n");
+        System.out.println("Vasa Volba: ");
+    }
 }
